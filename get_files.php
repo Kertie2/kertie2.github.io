bb4a5e4bf1b529604a5f00c7a8158fbeddda6204
Kertie2 <138666415+Kertie2@users.noreply.github.com>
2023-07-15 20:49:29 +0200

    Delete get_files.php

--- get_files.php
+++
@@ -1,6 +0,0 @@
-<?php
-$files = scandir($_SERVER['DOCUMENT_ROOT']);
-$files = array_diff($files, array('.', '..')); // Supprime les entrées '.' et '..'
-
-echo json_encode(array_values($files));
-?>
